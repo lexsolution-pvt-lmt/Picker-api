da97bedfbfc132e21f87f430ffc5ddcf7811ef17
Ravindu Dharmaraja <ravindudharmaraja00@gmail.com>
2023-01-05 12:33:08 +0530

    Wishlist(not completed)

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+
+class WishlistController extends Controller
+{
+   
+    public function index()
+    {
+       //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Wishlist $wishlist)
+    {
+        //
+    }
+
+    public function update(Request $request, Wishlist $wishlist)
+    {
+        //
+    }
+
+    
+    public function destroy(Wishlist $wishlist)
+    {
+        //
+    }
+}
