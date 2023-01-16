b40f57ffa864130d0115aa1eda9fe9d63246e837
Ravindu Dharmaraja <ravindudharmaraja00@gmail.com>
2023-01-16 09:45:22 +0530

    Final

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -71,12 +71,7 @@
         ***********************************-->
         <div class="content-body">
             <div class="container-fluid">
-                <div class="page-titles">
-					<ol class="breadcrumb">
-						<li class="breadcrumb-item"><a href="javascript:void(0)">Table</a></li>
-						<li class="breadcrumb-item active"><a href="javascript:void(0)">Bootstrap</a></li>
-					</ol>
-                </div>
+                
                 <!-- row -->
 
                 <div class="row">
